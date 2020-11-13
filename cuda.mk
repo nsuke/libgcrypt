b65ed2485cf223a6071220c86bc226c9e9e7ef48
Aki Sukegawa <nsukeg@gmail.com>
2020-11-13 17:04:31 +0900

    Use external ocb camellia cuda implementation

--- cuda.mk
+++ cuda.mk
@@ -1,5 +1,7 @@
 .cu.o:
-	$(NVCC) -std=c++17 --no-exceptions -o $@ -c $<
+	$(NVCC) -arch=sm_61 -std=c++17 --no-exceptions -O3 --default-stream per-thread -o $@ -c $<
 
 .cu.lo:
-	$(top_srcdir)/cudalt.py $@ $(NVCC) -std=c++17 --no-exceptions --compiler-options=\" $(CFLAGS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) \" -c $<
+	$(top_srcdir)/cudalt.py $@ $(NVCC) -arch=sm_61 -std=c++17 --no-exceptions -O3 --default-stream per-thread --compiler-options=\" $(CFLAGS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) \" -c $<
+
+CUDA_LIBS = -lcu_ocb -lcudart
